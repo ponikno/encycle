fe11b8eecbd3d5e3af85b697a441b7932498112f
ponikno <162648470+ponikno@users.noreply.github.com>
2024-03-14 23:27:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.getElementById("mainMenu");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
